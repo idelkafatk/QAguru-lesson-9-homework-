ceb0cf5f7585298e28178480a30dd426dcc89752
idelfatkhullin <maximbravin@gmail.com>
2022-12-24 23:10:01 +0600

    added owner properties

---
+++ src/test/java/tests/properties/OwnerTest.java
@@ -0,0 +1,22 @@
+package tests.properties;
+
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+
+public class OwnerTest {
+    public CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+    @Test
+    void readCredentialsTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+
+        String message = format("Log in with user %s and password %s", login, password);
+        System.out.println(message);
+    }
+}
