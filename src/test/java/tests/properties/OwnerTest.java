91b88a758f077b47a619e9a978f73819b4a109f3
idelfatkhullin <maximbravin@gmail.com>
2022-12-24 23:11:54 +0600

    added owner properties fixed

--- src/test/java/tests/properties/OwnerTest.java
+++ src/test/java/tests/properties/OwnerTest.java
@@ -2,10 +2,11 @@ package tests.properties;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
-
+@Tag("properties")
 public class OwnerTest {
     public CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
     @Test
